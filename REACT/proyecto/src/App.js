fe9e604e0c1c4ff6cb189ced6748d0ab216a4f1d
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-04-17 17:13:22 -0300

    Estilos CSS

--- REACT/proyecto/src/App.js
+++ REACT/proyecto/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './Componentes/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='contenedor-principal'>
+        <h1>Estos son nuestros conocidos de la vida</h1>
+        <Testimonio />
+      </div>
     </div>
   );
 }
---
+++ REACT/proyecto/src/Componentes/Testimonio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../Hojas-de-estlo/Testimonio.css';
+
+function Testimonio(props) {
+  return (
+    <div className='contenedor-testimonio'>
+      <img className='imagen-testimonio'
+      src={ require('../Imagenes/DeanWinchester.jpg')}
+      alt='Foto de Dean Winchester'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>{props.nombre} de {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.empresa}</p>
+        <p className='texto-testimonio'>{props.testimonio}</p>
+      </div>
+    </div>
+  );
+}
+
+
+export default Testimonio;
