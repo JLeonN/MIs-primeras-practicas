7313b04244cd4388319e42cfd9c6a96258a6df5d
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-04-19 22:40:34 -0300

    Primer etapa

--- REACT/proyecto-2-contador-de-clics/src/App.js
+++ REACT/proyecto-2-contador-de-clics/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import freeCodeCampLogo from './Imagenes/FreeCodeCamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+        className='freecodecamp-logo'
+        src={freeCodeCampLogo}
+        alt='Logo' />
+      </div>
+      <div className='contenedor-principal'>
+        
+      </div>
     </div>
   );
 }
