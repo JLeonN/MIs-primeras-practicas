53e6d7edeef8eb439d46c6f4d3630a0be07e2e60
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-04-21 23:07:46 -0300

    Primeros stilos

--- REACT/proyecto-3-calculadora/src/App.js
+++ REACT/proyecto-3-calculadora/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import freeCodeCampLogo from './Imagenes/FreeCodeCamp-logo.png'
 
 function App() {
   return (
-    <div className="App">
-      hola
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={freeCodeCampLogo}
+          className='freecodecamp-logo'
+          alt='Logo de Freecodecamp' />
+      </div>
     </div>
   );
 }
