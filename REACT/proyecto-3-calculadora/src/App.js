8e221e624268fd182d4b9005a7d06e78a53d8172
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-04-22 22:04:12 -0300

    Botones

--- REACT/proyecto-3-calculadora/src/App.js
+++ REACT/proyecto-3-calculadora/src/App.js
@@ -14,11 +14,31 @@ function App() {
       <div className='contenedor-calculadora'>
         <div className='fila'>
           <Boton>1</Boton>
+          <Boton>2</Boton>
+          <Boton>3</Boton>
+          <Boton>+</Boton>
         </div>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
-        <div className='fila'></div>
+        <div className='fila'>
+          <Boton>4</Boton>
+          <Boton>5</Boton>
+          <Boton>6</Boton>
+          <Boton>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
+        </div>
+        <div className='fila'>
+        <Boton>Borrar</Boton>
+        </div>
       </div>
     </div>
   );
