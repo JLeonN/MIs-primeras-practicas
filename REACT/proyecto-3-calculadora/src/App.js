d17a52793fdecdf53421b688e31c44640511faa1
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-04-24 22:10:53 -0300

    Pantalla

--- REACT/proyecto-3-calculadora/src/App.js
+++ REACT/proyecto-3-calculadora/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import freeCodeCampLogo from './Imagenes/FreeCodeCamp-logo.png'
 import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           alt='Logo de Freecodecamp' />
       </div>
       <div className='contenedor-calculadora'>
+        <Pantalla />
         <div className='fila'>
           <Boton>1</Boton>
           <Boton>2</Boton>
---
+++ REACT/proyecto-3-calculadora/src/componentes/Pantalla.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../Hoja-de-estilos/Pantalla.css'
+
+const Pantalla = ({ input }) => (
+  <div className='input'>
+    {input}
+  </div>
+);
+
+export default Pantalla;
