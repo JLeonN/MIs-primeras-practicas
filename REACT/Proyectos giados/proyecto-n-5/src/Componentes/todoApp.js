79a005d1bfbe7b2ae7051a94b205938112f47ada
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-05-25 19:38:31 -0300

    Piedra papel o tijera

--- REACT/Proyectos giados/proyecto-n-5/src/Componentes/todoApp.js
+++ REACT/Proyectos giados/proyecto-n-5/src/Componentes/todoApp.js
@@ -1,3 +1,30 @@
+import { useState } from "react";
+
 export default function TodoApp() {
-  return <div>Hola</div>;
+  const [title, setTitle] = useState ("Hola");
+
+  function handaleClick(e){
+    e.preventDefault();
+    setTitle("Yo.");
+  }
+
+  function handleChange(event) {
+    const value = event.target.value;
+
+    setTitle(value);
+  }
+
+  return (
+  <div className="todoContaimer">
+    <form className="todoCreateForm">
+      <input onChange={handleChange} className="todoInput" value={title} />
+      <input 
+      onClick={handaleClick}
+      type="submit" 
+      value="Create todo" 
+      className="buttonCreate" 
+      />
+    </form>
+  </div>
+  );
 }
