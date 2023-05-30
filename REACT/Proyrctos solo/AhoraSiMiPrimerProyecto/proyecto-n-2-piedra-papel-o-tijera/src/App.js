9fe4ad1b232314db5bb41c4ccbade6ffb82e2f48
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-05-29 22:04:53 -0300

    Dorso pronto

--- REACT/Proyrctos solo/AhoraSiMiPrimerProyecto/proyecto-n-2-piedra-papel-o-tijera/src/App.js
+++ REACT/Proyrctos solo/AhoraSiMiPrimerProyecto/proyecto-n-2-piedra-papel-o-tijera/src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import Inicio from '../src/Componentes/Inicio';
 import Dorso from './Componentes/Dorso';
 import Manos from './Componentes/Manos';
 
 function App() {
   return (
-    <section className='contenedorPrincipal'>
-      <h1 className='titulo'>Juego de Piedra, Papel o Tijera</h1>
-        <div className='juego'>
-          <div className='cartasReveladas'>
-            <Manos type="piedra" />
-          </div>
-          <div className='cartasReveladas'>
-            <Manos type="papel" />
-          </div>
-          <div className='cartasReveladas'>
-            <Manos type="tijera" />
-          </div>
-        </div>
-    </section>
+    <div>
+      <h1 className='col-12 text-center'>
+        Juego de piedra, papel o tijera
+      </h1>
+      <div className='row d-flex'>
+        <h2 className='dorsocss text-center display-1'>{Dorso[0].title}</h2>
+        <img src={Dorso[0].cartaEnDorso}
+          className='img-fluid dorso' alt='Dorso de carta'/>
+      </div>
+    </div>
   );
 }
 
--- REACT/Proyrctos solo/AhoraSiMiPrimerProyecto/proyecto-n-2-piedra-papel-o-tijera/src/Componentes/Dorso.js
+++ REACT/Proyrctos solo/AhoraSiMiPrimerProyecto/proyecto-n-2-piedra-papel-o-tijera/src/Componentes/Dorso.js
@@ -2,12 +2,26 @@ import React from "react";
 import '../Hojas-de-estilos/Dorso.css'
 import dorso from '../Imagenes/Dorso.jpg';
 
-function Dorso() {
-  return (
-    <div className="contenedor">
-      <img src={dorso} alt="Dorso de la carta." />
-    </div>
-  );
-}
+export default [
+  { 'cartaEnDorso':dorso,
+    'title': '?'
+  }
+]
 
-export default Dorso;
+
+
+
+
+
+
+
+
+// function Dorso() {
+//   return (
+//     <div className="contenedor">
+//       <img src={dorso} alt="Dorso de la carta." />
+//     </div>
+//   );
+// }
+
+// export default Dorso;
