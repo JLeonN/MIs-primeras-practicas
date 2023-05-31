8f84ef95419e822cbf246863b184085cc3ebb8f7
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-05-30 22:52:15 -0300

    Selector de cartas

--- REACT/Proyrctos solo/AhoraSiMiPrimerProyecto/proyecto-n-2-piedra-papel-o-tijera/src/App.js
+++ REACT/Proyrctos solo/AhoraSiMiPrimerProyecto/proyecto-n-2-piedra-papel-o-tijera/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
@@ -6,6 +7,29 @@ import Dorso from './Componentes/Dorso';
 import Manos from './Componentes/Manos';
 
 function App() {
+  const [mostrarPiedra, setMostrarPiedra] = useState(true);
+  const [mostrarPapel, setMostrarPapel] = useState(true);
+  const [mostrarTijera, setMostrarTijera] = useState(true);
+
+  const handleClicPiedra = () => {
+    setMostrarPiedra(true);
+    setMostrarPapel(false);
+    setMostrarTijera(false);
+  };
+
+  const handleClicPapel = () => {
+    setMostrarPiedra(false);
+    setMostrarPapel(true);
+    setMostrarTijera(false);
+  };
+
+  const handleClicTijera = () => {
+    setMostrarPiedra(false);
+    setMostrarPapel(false);
+    setMostrarTijera(true);
+  };
+
+
   return (
     <div className='vh-100'>
       <h1 className='col-12 text-center'>
@@ -17,19 +41,32 @@ function App() {
           className='img-fluid dorso' alt='Dorso de carta'/>
       </div>
       <div className='d-flex'>
-        <h2 className='dorsocss text-center display-1'>{Manos[0].title}</h2>
-        <img src={Manos[0].piedra}
-          className='img-fluid dorso' alt='Piedra'/>
+        {mostrarPiedra && (
+          <>
+            <h2 className='dorsocss text-center display-1'>{Manos[0].title}</h2>
+            <img src={Manos[0].piedra}
+              className='img-fluid dorso' alt='Piedra'
+              onClick={handleClicPiedra}/>
+          </>
+        )}
 
-          
-        <h2 className='dorsocss text-center display-1'>{Manos[1].title}</h2>
-        <img src={Manos[1].papel}
-          className='img-fluid dorso' alt='Papel'/>
+        {mostrarPapel && (
+          <>
+            <h2 className='dorsocss text-center display-1'>{Manos[1].title}</h2>
+            <img src={Manos[1].papel}
+              className='img-fluid dorso' alt='Papel'
+              onClick={handleClicPapel}/>
+          </>
+        )}
 
-          
-        <h2 className='dorsocss text-center display-1'>{Manos[2].title}</h2>
-        <img src={Manos[1].tijera}
-          className='img-fluid dorso' alt='Tijera'/>
+        {mostrarTijera && (
+          <>
+            <h2 className='dorsocss text-center display-1'>{Manos[2].title}</h2>
+            <img src={Manos[2].tijera}
+              className='img-fluid dorso' alt='Tijera'
+              onClick={handleClicTijera}/>
+          </>
+        )}
       </div>
     </div>
   );
