db408466dbef0169360f6dd286bcb08ea5fee9e4
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-06-08 22:19:06 -0300

    Navegacion proyecto

---
+++ REACT/Proyrctos solo/AhoraSiMiPrimerProyecto/proyecto-n-3-navegacion/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hola</h1>
+      hola
+    </div>
+  );
+}
+
+export default App;
