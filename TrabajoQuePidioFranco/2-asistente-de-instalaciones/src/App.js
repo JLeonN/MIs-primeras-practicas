c95fc4ebf876846169b336fa8385c9fb2a5ec7e5
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-06-16 23:13:30 -0300

    Primer componente

--- TrabajoQuePidioFranco/2-asistente-de-instalaciones/src/App.js
+++ TrabajoQuePidioFranco/2-asistente-de-instalaciones/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Estructura from './Componentes/Estructura';
 
 function App() {
   return (
     <div className="App">
-      hola
+        <Router>
+      <Estructura 
+      backgroundColor="red"
+      titleColor="Rojo"
+      title="Asistente de instalaciones"
+      description="Un texto de ejemplo rápido para construir sobre el título de la tarjeta y componer la mayor parte del contenido de la tarjeta."
+      link="/ruta-del-enlace"
+      boton="Iniciar"
+      />
+        </Router>
     </div>
   );
 }
---
+++ TrabajoQuePidioFranco/2-asistente-de-instalaciones/src/Componentes/Estructura.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Estructura(props) {
+  const { backgroundColor, titleColor, title, description, link, boton } = props;
+
+  return (
+    <div className="card container p-3 m-3 mx-auto" style={{ width: '18rem' }}>
+      <div style={{ backgroundColor }}>
+        <h3 className="text-center">{titleColor}</h3>
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <Link to={link} className="btn btn-primary">{boton}</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Estructura;
