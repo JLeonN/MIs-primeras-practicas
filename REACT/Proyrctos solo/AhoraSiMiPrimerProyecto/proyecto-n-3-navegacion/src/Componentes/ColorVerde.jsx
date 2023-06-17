061fd3a2930ee98d366ab9b58c80f3eb83b37b21
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-06-16 21:50:22 -0300

    Termine Pruebas

--- REACT/Proyrctos solo/AhoraSiMiPrimerProyecto/proyecto-n-3-navegacion/src/Componentes/ColorVerde.jsx
+++ REACT/Proyrctos solo/AhoraSiMiPrimerProyecto/proyecto-n-3-navegacion/src/Componentes/ColorVerde.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function ColorVerde() {
+  const [checkboxChecked, setCheckboxChecked] = useState(false);
+
+  const handleCheckboxChange = (event) => {
+    setCheckboxChecked(event.target.checked);
+  };
+
   return (
     <div className='container' style={{ backgroundColor: 'green' }}>
-      <p class="text-success">Verde</p>
-      <Link className="btn btn-light" to="/">Volver al inicio
-      </Link>
+      <p className="text-success">Verde</p>
+      <div className=''>
+        <input
+          type="checkbox"
+          name="casilla"
+          checked={checkboxChecked}
+          onChange={handleCheckboxChange}
+        />{' '}
+        Seleccione para ir al inicio
+      </div>
+      <Link className="btn btn-light" to={checkboxChecked ? "/" : "#"}>Volver al inicio</Link>
     </div>
   );
 }
