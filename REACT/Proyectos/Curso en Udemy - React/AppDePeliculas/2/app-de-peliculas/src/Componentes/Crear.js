d40bc9a4eabed14682d0cf838e117a19d8530323
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-07-24 07:14:03 -0300

    JS-2

--- REACT/Proyectos/Curso en Udemy - React/AppDePeliculas/2/app-de-peliculas/src/Componentes/Crear.js
+++ REACT/Proyectos/Curso en Udemy - React/AppDePeliculas/2/app-de-peliculas/src/Componentes/Crear.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Crear = () => {
-  const titulo = "Añadir pelicula";
+  const tituloComponente = "Añadir pelicula";
+  const [peliState, setPeliState] = useState({
+    titulo: '',
+    describe: ''
+  });
+
+  const { titulo, descripcion } = peliState;
 
   const conseguirDatosForm = e => {
     e.preventDefault();
@@ -11,14 +17,39 @@ export const Crear = () => {
     let titulo = target.title.value;
     let descripcion = target.descripcion.value;
 
-    // let peli = {
-    //     id:
-    // }
+    // Crear objeto de la pelicula a guardar
+    let peli = {
+      id: new Date().getTime(),
+      titulo,
+      descripcion
+    };
+
+    // Guardar estado
+    setPeliState(peli);
+
+    // Guardar en el almacenamiento local
+    guardarEnnnStorage(peli);
+  }
+
+  const guardarEnnnStorage = peli => {
+
+    // Conseguir los elementos que ya tennemos enn Localstorage
+
+    // comprobar si es una array
+
+    // guardar enn el Localstorage
+
+    // devolver objeto guardado
+
   }
 
   return (
     <div className="add">
-      <h3 className="title">{titulo}</h3>
+      <h3 className="title">{tituloComponente}</h3>
+
+      <strong>
+        {(titulo && descripcion) && "Has creado la pelicula: " + titulo}
+      </strong>
 
       <form onSubmit={conseguirDatosForm}>
         <input type="text"
