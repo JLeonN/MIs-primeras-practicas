912d113c51259c7f997584584d51167369fd588d
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-08-26 16:05:35 -0300

    Navegacion

--- REACT/Proyectos/Curso en Udemy - React/rutas/src/App.js
+++ REACT/Proyectos/Curso en Udemy - React/rutas/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {Curriculum} from "./Comonentes/Curriculum";
+import { MisRutas } from './Router/MisRutas';
 
 function App() {
   return (
-    <div>
-      <Curriculum />
-    </div>
+    <>
+    <MisRutas />
+    </>
   );
 }
 
--- REACT/Proyectos/Curso en Udemy - React/rutas/src/Router/MisRutas.js
+++ REACT/Proyectos/Curso en Udemy - React/rutas/src/Router/MisRutas.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Routes, Route, BrowserRouter, NavLink } from 'react-router-dom';
+import {Inicio} from "../Comonentes/Inicio";
+import {Portafolio} from "../Comonentes/Portafolio";
+import {Servicios} from "../Comonentes/Servicios";
+import {Curriculum} from "../Comonentes/Curriculum";
+import {Contacto} from "../Comonentes/Contacto";
 
 export const MisRutas = () => {
   return (
-    <div>MisRutas</div>
+    <BrowserRouter>
+      {/* Header y Navegacion */}
+
+      {/* Contenido central */}
+      <Routes>
+        <Route path='/' element={<Inicio />} />
+        <Route path='/inicio' element={<Inicio />} />
+        <Route path='/portafolio' element={<Portafolio />} />
+        <Route path='/servicios' element={<Servicios />} />
+        <Route path='/curriculum' element={<Curriculum />} />
+        <Route path='/contacto' element={<Contacto />} />
+      </Routes>
+
+      {/* Footer */}
+    </BrowserRouter>
   )
 }
