dcfc76e7437f04d238d8551806ee9933dd703f49
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-09-11 18:02:29 -0300

    Trabajo que pidio Franco saludar

---
+++ TrabajoQuePidioFranco/Ejercicios de JavaScript/Nivel facil/Saludo.js
@@ -0,0 +1,17 @@
+// El primer ejercicio es este:
+
+// 0) Escribir una funcion para saludar a una persona.
+// Requisitos:
+//  - La funcion debe recibir un nombre
+//  - La funcion debe retornar el saludo de esta forma:
+
+// "Hola, Jorge!"
+
+function saludar() {
+  document.getElementById('saludar'); aca.innerText = 'hola Jorge'
+}
+
+
+// function saludar() {
+//   document.write("Hola Jorge")
+// };
