d8cc045f26a107247275978e5e27136e5972d6dd
J-Leo-N <124943644+JLeonN@users.noreply.github.com>
2023-10-09 21:45:52 -0300

    Calcular

---
+++ TrabajoQuePidioFranco/Ejercicios de JavaScript/Nivel facil/Funciones/Calcular.js
@@ -0,0 +1,10 @@
+function calcular(operacion, num1, num2) {
+  let x = "+";
+  if (operacion === "+") {
+    return sumar(num1, num2);
+  }
+};
+
+
+console.log(calcular(50, 25));
+"operacion"
--- TrabajoQuePidioFranco/Ejercicios de JavaScript/Nivel facil/Funciones/Funciones-S-R-D-M.js
+++ TrabajoQuePidioFranco/Ejercicios de JavaScript/Nivel facil/Funciones/Funciones-S-R-D-M.js
@@ -1,20 +1,38 @@
-function sumar (num1,num2) {
+function sumar(num1, num2) {
   return num1 + num2;
 };
 
-function restar (num1,num2) {
+function restar(num1, num2) {
   return num1 - num2;
 };
 
-function dividir (num1,num2) {
+function dividir(num1, num2) {
   return num1 / num2;
 };
 
-function multiplicar (num1,num2) {
+function multiplicar(num1, num2) {
   return num1 * num2;
 };
 
-console.log (sumar(50,25));
-console.log (restar(90,15));
-console.log (dividir(150,2));
-console.log (multiplicar(5,15));
+let resultadoSumar = sumar(50, 25);
+let resultadoRestar = restar(90, 15);
+let resultadoDividir = dividir(150, 2);
+let resultadoMultiplicar = multiplicar(5, 15);
+
+
+function calcular(num1, operacion, num2) {
+  if (operacion == "+") {
+    return sumar(num1, num2);
+  }
+  if (operacion == "-") {
+    return restar(num1, num2);
+  }
+  if (operacion == "/") {
+    return dividir(num1, num2);
+  }
+  if (operacion == "*") {
+    return multiplicar(num1, num2);
+  }
+}
+
+console.log(calcular(50, "+", 25));
