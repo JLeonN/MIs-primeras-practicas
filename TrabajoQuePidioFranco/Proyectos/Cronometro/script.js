3b2a304027841ec551280b59deac615b3fbe44ca
Leonardo Núñez <124943644+JLeonN@users.noreply.github.com>
2024-01-23 07:22:18 -0300

    Cronometro - 3

--- TrabajoQuePidioFranco/Proyectos/Cronometro/script.js
+++ TrabajoQuePidioFranco/Proyectos/Cronometro/script.js
@@ -1,6 +1,6 @@
 const stopwatch = document.getElementById('stopwatch');
 const playPauseButton = document.getElementById('play-pause');
-const secondSphere = document.getElementById('seconds-sphere');
+const secondsSphere = document.getElementById('seconds-sphere');
 
 let stopwatchInterval;
 let runningTime = 0;
@@ -17,16 +17,35 @@ const playPause = () => {
 }
 
 const pause = () => {
-
+  secondsSphere.style.animationPlayState = 'paused';
+  clearInterval(stopwatchInterval);
 }
 
+const stop = () => {
+  secondsSphere.style.transform = 'rotate(90deg) transleteX(60px)';
+  secondsSphere.style.animation = 'none';
+  playPauseButton.classList.remove('running');
+  runningTime = 0;
+  clearInterval(stopwatchInterval);
+  stopwatch.textContent = '00:00';
+}
 
 const start = () => {
   secondsSphere.style.animation = 'rotacion 60s linear infinite';
-  let startTime = date.now() - runningTime;
+  let startTime = Date.now() - runningTime;
   secondsSphere.style.animationPlayState = 'running';
-  stopwatchInterval = setInterval( () => {
-    runningTime = date.now() - startTime;
+  stopwatchInterval = setInterval(() => {
+    runningTime = Date.now() - startTime;
     stopwatch.textContent = calculateTime(runningTime);
-  },1000)
+  }, 1000)
+}
+
+const calculateTime = runningTime => {
+  const total_seconds = Math.floor(runningTime / 1000);
+  const total_minutes = Math.floor(total_seconds / 60);
+
+  const display_seconds = (total_seconds % 60).toString().padStart(2, "0");
+  const display_minutes = total_minutes.toString().padStart(2, "0");
+
+  return `${display_minutes}:${display_seconds}`
 }
